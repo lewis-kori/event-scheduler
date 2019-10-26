7274a0a1c34c9c08e4c2a48b2c7450f423837ea9
lewis <lewiskosh@gmail.com.com>
2019-10-26 12:33:04 +0300

    better commenting added on the accounts test file

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,16 +5,20 @@ from django.contrib.auth.models import User
 from .models import userProfile
 
 # Create your tests here.
+
+# test the user registration endpoint
 class RegistrationTestCase(APITestCase):
     def test_registration(self):
         data={"username":"lynn","password":"PASwwordLit","email":"lynn@gmail.com"}
         response=self.client.post('/auth/users/',data)
         self.assertEqual(response.status_code,status.HTTP_201_CREATED)
-
+# test case for the userprofile model
 class userProfileTestCase(APITestCase):
     profile_list_url=reverse('all-profiles')
     def setUp(self):
+        # create a new user making a post request to djoser endpoint
         self.user=self.client.post('/auth/users/',data={'username':'mario','password':'i-keep-jumping'})
+        # obtain a json web token for the newly created user
         response=self.client.post('/auth/jwt/create/',data={'username':'mario','password':'i-keep-jumping'})
         self.token=response.data['access']
         self.api_authentication()
@@ -22,24 +26,28 @@ class userProfileTestCase(APITestCase):
     def api_authentication(self):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer '+self.token)
 
+    # retrieve a list of all user profiles while the request user is authenticated
     def test_userprofile_list_authenticated(self):
         response=self.client.get(self.profile_list_url)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
-    
+
+    # retrieve a list of all user profiles while the request user is unauthenticated
     def test_userprofile_list_unauthenticated(self):
         self.client.force_authenticate(user=None)
         response=self.client.get(self.profile_list_url)
         self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
 
+    # check to retrieve the profile details of the authenticated user
     def test_profile_detail_retrieve(self):
         response=self.client.get(reverse('profile',kwargs={'pk':1}))
-        # print(response.data)
+        print(response.data)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
-    
+
+    # populate the user profile that was automatically created using the signals
     def test_populate_profile(self):
         profile_data={'description':'I am a very famous game character','location':'nintendo world','is_creator':'True',}
         response=self.client.put(reverse('profile',kwargs={'pk':1}),data=profile_data)
         print(response.data)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
 
-    # def test_delete_user(self):
+    
